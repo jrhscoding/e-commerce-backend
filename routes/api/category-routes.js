8b4fdfdd7eef62e0aaa853140a313f7d1468661c
joseph <j.hernandez.92189@gmail.com>
2022-11-08 09:32:31 -0800

    added general get routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,36 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  // STILL NEED TO ************* include its associated Products
+  try {
+    const allCategories = await Category.findAll();
+    res
+      .status(200)
+      .json(allCategories);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  // STILL NEED ****************** to include its associated Products
+  try {
+    const singleCategory = await Category.findAll({
+      where: id = req.body.id,
+    })
+    res
+      .status(200)
+      .json(singleCategory)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error)
+  }
+
 });
 
 router.post('/', (req, res) => {
